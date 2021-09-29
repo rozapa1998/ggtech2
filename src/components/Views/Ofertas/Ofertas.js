95431f184f3f29c4b6f589ef030baf75b25eab29
rozapa1998 <rodrigo1998.rz@gmail.com>
2021-09-29 20:18:19 -0300

    filtro ofertas

--- src/components/Views/Ofertas/Ofertas.js
+++ src/components/Views/Ofertas/Ofertas.js
@@ -1,9 +1,39 @@
-import React from 'react'
+import React, {useEffect, useState} from 'react'
+import { db } from '../../../firebase';
+import Item from '../../ItemListContainer/Item';
 
 const Ofertas = () => {
+    
+    const [productosO, setProductosO] = useState([])
+    
+    const getProducts = () =>{
+            const docs = [];
+            db.collection("products")
+            .where("oferta","==",true)
+            .onSnapshot((querySnapshot)=>{
+                querySnapshot.forEach((doc)=>{
+                    docs.push({...doc.data(), id: doc.id});
+                });
+                setProductosO(docs)
+            });
+        
+    };
+
+   useEffect(() => {
+       getProducts();
+   }, []);
+
     return (
-        <div>
-            
+        <div className= "Product container">
+            <div className="row ms-5">
+                {productosO.map(producto => {
+                return(
+                    <Item
+                    key={producto.id}
+                    item={producto}/>
+                )}
+                )}
+                </div>
         </div>
     )
 }
